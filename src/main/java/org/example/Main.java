ab07675d4b4d11c0c3d3d77162c14f21b0b4fcc4
Zafarali Tolibov <tolibovzafarali7@gmail.com>
2025-06-08 18:55:12 -0500

    Completed the practice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,10 +14,10 @@ public class Main {
         // Use the static methods you wrote in the Calculator class
         // to assign proper values to the four variables below
         // (instead of 0)
-        int sum = 0;
-        int difference = 0;
-        int product = 0;
-        int quotient = 0;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        int quotient = a / b;
 
         System.out.println("The sum is " + sum);
         System.out.println("The difference is " + difference);
